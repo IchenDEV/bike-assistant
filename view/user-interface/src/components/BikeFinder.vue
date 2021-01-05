5bf6e4799b9055fc56f6dfa96bcd3e1b927e6baf
idevlab <idevlab@outlook.com>
2021-01-05 18:19:28 +0800

    fix some bugs

--- view/user-interface/src/components/BikeFinder.vue
+++ view/user-interface/src/components/BikeFinder.vue
@@ -2,6 +2,7 @@
   <div class="patrol_area">
     <div id="allmap" style="height:100vh;margin-top:5px;"></div>
     <v-btn fab @click="$router.back()" class="back-btn">Back</v-btn>
+    <v-btn fab @click="switchFrontLight" class="back-btn-2">Light</v-btn>
   </div>
 </template>
 
@@ -27,36 +28,53 @@ export default {
     Start() {
       this.map = new BMapGL.Map("allmap");
       let map = this.map;
-      map.centerAndZoom(new BMapGL.Point(121.814224, 31.156484), 17);
+      map.centerAndZoom(new BMapGL.Point(121.814224, 31.156484), 10);
       map.enableScrollWheelZoom(true);
 
       this.geolocation = new BMapGL.Geolocation();
-      this.timer = setInterval(this.updatePos, 1000);
+      this.timer = setInterval(this.updatePos, 10000);
       this.driving = new BMapGL.WalkingRoute(this.map, {
         renderOptions: { map: this.map, autoViewport: false },
       });
+    },
+     async switchFrontLight() {
+      this.frontLight = !this.frontLight;
+      await this.$store.dispatch("updateLight1", this.frontLight);
     },
     updatePos() {
       let that = this;
       this.geolocation.getCurrentPosition(
         async function(r) {
           if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-            
             that.mk = new BMapGL.Marker(r.point);
-            that.map.panTo(r.point);
+
+            that.map.clearOverlays();
             that.map.addOverlay(that.mk);
             try {
               await that.$store.dispatch("updateGeo");
-              var p1 = r.point;
-              var p2 = new BMapGL.Point(
-                that.$store.state.environment.geo.lat,
-                that.$store.state.environment.geo.lng
+              let p1 = r.point;
+
+              let p2 = new BMapGL.Point(
+                that.$store.state.environment.geo.lng,
+                that.$store.state.environment.geo.lat
               );
 
-              that.driving.search(p1, p2);
+              let convertor = new BMap.Convertor();
+              let pointArr = [];
+              pointArr.push(p2);
+              convertor.translate(pointArr, 1, 5, (data) => {
+                if (data.status === 0) {
+                  that.map.panTo(data.points[0]);
+                  let p2 = new BMapGL.Point(
+                    data.points[0].lng,
+                    data.points[0].lat
+                  );
+                  let mk = new BMapGL.Marker(p2);
+                  that.map.addOverlay(p2);
+                  that.driving.search(p1, p2);
+                }
+              });
             } catch {}
-          } else {
-            alert("failed" + this.getStatus());
           }
         },
         { enableHighAccuracy: true }
@@ -77,6 +95,12 @@ export default {
 .back-btn {
   position: absolute;
   bottom: 0;
+  z-index: 10;
+  left: 1rem;
+}
+.back-btn-2 {
+  position: absolute;
+  bottom: 4rem;
   z-index: 10;
   left: 1rem;
 }
--- view/user-interface/src/components/TrackMap.vue
+++ view/user-interface/src/components/TrackMap.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="patrol_area">
-    <div id="allmap" style="height:600px;margin-top:5px;"></div>
+    <div id="allmap" style="height:100vh;margin-top:5px;"></div>
+    <v-btn fab @click="$router.back()" class="back-btn">Back</v-btn>
+    <v-btn fab @click="switchFrontLight" class="back-btn-2">Light</v-btn>
   </div>
 </template>
 
@@ -11,7 +13,7 @@ export default {
     return {
       points: null,
       maps: "",
-      pl: "",
+      pl: null,
       timer: null,
       mk: null,
     };
@@ -20,41 +22,56 @@ export default {
     this.pathStart();
   },
   methods: {
+    async switchFrontLight() {
+      this.frontLight = !this.frontLight;
+      await this.$store.dispatch("updateLight1", this.frontLight);
+    },
     async pathStart() {
       this.maps = new BMapGL.Map("allmap");
-
       this.maps.enableScrollWheelZoom(true);
-
+      this.maps.centerAndZoom(new BMapGL.Point(121.814224, 31.156484), 10);
       await this.$store.dispatch("updateGeo");
-      this.pl = new BMapGL.Polyline([
-        new BMapGL.Point(this.$store.state.environment.geo.lat,
-                this.$store.state.environment.geo.lng),
-        new BMapGL.Point(this.$store.state.environment.geo.lat,
-                this.$store.state.environment.geo.lng),
-      ]);
-      this.maps.addOverlay(this.pl);
-
-      let list = this.pl.getPath();
-      list.push(new BMapGL.Point(this.$store.state.environment.geo.lat,
-                this.$store.state.environment.geo.lng));
-      this.pl.setPath(list);
-
-      this.mk = new BMapGL.Marker(new BMapGL.Point(this.$store.state.environment.geo.lat,
-                this.$store.state.environment.geo.lng));
-
-      this.maps.addOverlay(this.mk);
       this.timer = setInterval(this.updatePotion, 1000);
     },
     async updatePotion() {
       await this.$store.dispatch("updateGeo");
-      let list = this.pl.getPath();
-      console.log(this.$store.state.environment.geo)
-      const point=new BMapGL.Point(this.$store.state.environment.geo.lat,
-                this.$store.state.environment.geo.lng);
-      list.push(point);
-      this.pl.setPath(list);
-      this.maps.panTo(point);
 
+      const point = new BMapGL.Point(
+        this.$store.state.environment.geo.lng,
+        this.$store.state.environment.geo.lat
+      );
+
+      if (this.pl == null || this.pl == undefined) {
+        let convertor = new BMap.Convertor();
+        let pointArr = [];
+        pointArr.push(point);
+        convertor.translate(pointArr, 1, 5, (data) => {
+          this.pl = new BMapGL.Polyline([
+            new BMapGL.Point(
+              data.points[0].lng,
+              data.points[0].lat
+            ),
+            new BMapGL.Point(
+              data.points[0].lng,
+              data.points[0].lat
+            ),
+          ]);
+          this.maps.addOverlay(this.pl);
+        });
+        return
+      }
+
+      let list = this.pl.getPath();
+      let convertor = new BMap.Convertor();
+      let pointArr = [];
+      pointArr.push(point);
+      convertor.translate(pointArr, 1, 5, (data) => {
+        if (data.status === 0) {
+          list.push(data.points[0]);
+          this.pl.setPath(list);
+          this.maps.panTo(data.points[0]);
+        }
+      });
     },
   },
   beforeDestroy() {
@@ -70,5 +87,22 @@ export default {
 .text {
   font-size: 30px;
   line-height: 46px;
+}
+#allmap {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+}
+.back-btn {
+  position: absolute;
+  bottom: 0;
+  z-index: 10;
+  left: 1rem;
+}
+.back-btn-2 {
+  position: absolute;
+  bottom: 4rem;
+  z-index: 10;
+  left: 1rem;
 }
 </style>
